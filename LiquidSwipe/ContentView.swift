cb7aa660d90cabcebc9513e9a5c5b98d48d65819
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2024-10-18 16:29:18 +0600

    Initial Commit

---
+++ LiquidSwipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiquidSwipe
+//
+//  Created by Shishir_Mac on 18/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiquidSwipe/LiquidSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidSwipeApp.swift
+//  LiquidSwipe
+//
+//  Created by Shishir_Mac on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LiquidSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
